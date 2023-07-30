253abb7e7abb8af03dabf2951e140eb1edc03c48
Ivan Avila <ivanavilar456@gmail.com>
2023-07-29 19:03:34 -0600

    se hace correcion del guardado

--- detalle.php
+++ detalle.php
@@ -132,9 +132,9 @@ include 'template/header.php';
                                             </div>
                                         </td>
                                     </tr>
-                                <?php
+                                    <?php
                                 }
-                                ?>
+                                ?> 
                             </tbody>
                         </table>
 
@@ -151,8 +151,8 @@ include 'template/header.php';
                             <div class="modal-body">
                                 <form id="formulario_detalle" enctype="multipart/form-data" action="guardar_detalle.php" method="POST">
 
-                                <input type="hidden" id="id_cliente" name="id_cliente" value="<?php echo $id_cliente; ?>">
-
+                                <input type="text" id="id_cliente" name="id_cliente" value="<?php echo $id_cliente; ?>">
+                                           
                                     <div class="row">
                                         <div class="col-md-6">
                                             <label for="equipo">Equipo</label>
